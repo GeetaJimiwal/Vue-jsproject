9f07d7bcce2301df631ad4a524a4b6baefaa77e8
GeetaBora <geetaborajimiwal123@gmail.com>
2022-04-27 23:21:28 +0530

    add component

---
+++ src/App.vue
@@ -0,0 +1,26 @@
+<template>
+<div>
+<HelloWorld />
+   <HomeComponent/>
+</div>
+   
+  
+ 
+</template>
+
+<script>
+import HelloWorld from './components/HelloWorld.vue'
+ import HomeComponent from './components/HomeComponent.vue' 
+ 
+
+export default {
+  name: 'App',
+  components: {
+    HelloWorld,
+    HomeComponent,
+   
+  },
+}
+</script>
+
+ 
---
+++ src/components/HomeComponent.vue
@@ -0,0 +1,28 @@
+<template>
+  <div>
+    <h1>Email {{email}}</h1>
+    <h1>Mobile{{mobile}}</h1>
+
+    <button v-on:click="getdata('button clicked')">click me</button>
+  </div>
+     
+</template>
+
+<script>
+export default {
+  name: 'HomeComponent',
+  data(){
+   
+        return{email: "geeta@gmail.com",
+        mobile:  13563}
+   
+    },
+    methods:{
+        getdata(data){
+        alert (data);
+        }
+    }
+  
+ 
+}
+</script>
